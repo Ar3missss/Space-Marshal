186fc897a2b40b0d3f31254ee9c98ed64d7b2c34
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-26 16:54:45 +0530

    requiremetns

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 
 WIDTH=1000
 HEIGHT=600
-
+BORDER= pygame.Rect(WIDTH/2-5,0,10,HEIGHT)
 WIN=pygame.display.set_mode((WIDTH,HEIGHT))
 TITLE=pygame.display.set_caption('Space Marshal')
 WHITE = (255,255,255)
+BLACK =(0 ,0 ,0)
 FPS=60
 VEL=5
+BULLET_VEL=7
+MAX_BULLETS=3
 SPACESHIP_WIDTH=55
 SPACESHIP_HEIGHT=40
 
@@ -16,18 +19,22 @@ YELLOW_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP
 RED_SPACESHIP_IMAGE=pygame.image.load("/Users/macbookair/Python/space_marshall/Assets/spaceship_red.png")
 RED_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
 
+
+## DRAW
 def draw_window(yellow,red):                            ## draw fxn is used only to draw or u can say only to display it does not create any objects so we passed yellow and red
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN,BLACK,BORDER )
     WIN.blit(YELLOW_SPACESHIP,(yellow.x,yellow.y))
     WIN.blit(RED_SPACESHIP,(red.x,red.y))               ## Blit is used to display text and images on screen
     pygame.display.update()
+      
 
-
+## SPACESHIPS MOVEMENTS
 def yellow_movement(keys_pressed,yellow):
     if keys_pressed[pygame.K_a] and yellow.x - VEL>=0 :         # left
             yellow.x -= VEL
 
-    if keys_pressed[pygame.K_d] and yellow.x+VEL+SPACESHIP_HEIGHT<=WIDTH:  # Right
+    if keys_pressed[pygame.K_d] and yellow.x+VEL+SPACESHIP_HEIGHT<=BORDER.x:  # Right
             yellow.x += VEL
 
     if keys_pressed[pygame.K_w] and yellow.y - VEL>=0:      # UP
@@ -38,7 +45,7 @@ def yellow_movement(keys_pressed,yellow):
 
 
 def red_movement(keys_pressed,red):
-    if keys_pressed[pygame.K_LEFT] and red.x - VEL>=0 :         # left
+    if keys_pressed[pygame.K_LEFT] and red.x -VEL>=BORDER.x+BORDER.width :         # left
             red.x -= VEL
 
     if keys_pressed[pygame.K_RIGHT] and red.x+VEL+SPACESHIP_HEIGHT<=WIDTH:  # Right
@@ -50,10 +57,15 @@ def red_movement(keys_pressed,red):
     if keys_pressed[pygame.K_DOWN] and red.y +VEL+SPACESHIP_WIDTH<= HEIGHT:  # Down 
             red.y += VEL                 
 
+
+## MAIN LOOP
 def main():
     yellow=pygame.Rect(100,HEIGHT/2,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)                ## Rect fxn create your object it stores the posistion,width and height of ur object 
     red= pygame.Rect(900-SPACESHIP_WIDTH,HEIGHT/2,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
-
+    
+    yellow_bullets=[]
+    red_bullets=[]
+    
     clock=pygame.time.Clock()                                            ## It is used to maintain a constant FPS in all systems
     run = True
     while run:
@@ -62,6 +74,15 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LCTRL and len(yellow_bullets)<MAX_BULLETS:
+                Bullet=pygame.Rect(yellow.x+yellow.width,yellow.y+yellow.height/2-2,10,5)
+                yellow_bullets.append(Bullet)
+            
+            if event.key == pygame.K_RCTRL and len(red_bullets)<MAX_BULLETS:
+                Bullet=pygame.Rect(red.x+red.y+red.height/2-2,10,5)
+                red_bullets.append(Bullet)            
+        print(red_bullets,yellow_bullets)
         keys_pressed=pygame.key.get_pressed()
         yellow_movement(keys_pressed,yellow)
         red_movement(keys_pressed,red)
